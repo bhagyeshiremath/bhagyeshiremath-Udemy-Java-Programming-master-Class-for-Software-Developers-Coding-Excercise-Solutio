8de363dd41d71cdb3d9cd9b0ebfb4a46e303bf0e
Bhagyesh Hiremath <65004269+bhagyeshiremath@users.noreply.github.com>
2021-04-09 18:51:53 +0530

    Create DecimalComparator.java

---
+++ Coding Exercise : 5/DecimalComparator.java
@@ -0,0 +1,10 @@
+public class DecimalComparator
+{
+    public static boolean areEqualByThreeDecimalPlaces (double a,double b) 
+    {
+        if((int)a * == b1)
+            return true;
+        else
+            return false;
+    }
+}

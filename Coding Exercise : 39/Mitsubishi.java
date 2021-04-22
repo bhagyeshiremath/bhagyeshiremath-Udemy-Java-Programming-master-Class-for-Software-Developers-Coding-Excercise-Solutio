d5630c8f1f410068e301491d595d0d6d4d10e39e
Bhagyesh Hiremath <65004269+bhagyeshiremath@users.noreply.github.com>
2021-04-22 10:54:35 +0530

    Create Mitsubishi.java

---
+++ Coding Exercise : 39/Mitsubishi.java
@@ -0,0 +1,23 @@
+public Mitsubishi extends Car
+{
+    public Mitsubishi(String name, int cylinders) {
+        super(name, cylinders);
+        this.wing = true;
+    }
+
+    @Override
+    public void startEngine() 
+    {
+        System.out.println("Ferrari.startEngine() called");
+    }
+    
+     public void accelatrate() 
+    {
+        System.out.println("Ferrari.startEngine() called");
+    }
+    
+     public void startEngine() 
+    {
+        System.out.println("Ferrari.startEngine() called");
+    }
+}

0a71284537c4a12aeaf9c4fd6a0420d30bbf4fa2
Bhagyesh Hiremath <65004269+bhagyeshiremath@users.noreply.github.com>
2021-04-22 10:51:24 +0530

    Create Ford.java

---
+++ Coding Exercise : 39/Ford.java
@@ -0,0 +1,13 @@
+public class Ford extends Car 
+{
+    public Ford(String name, int cylinders) 
+    {
+        super(name, cylinders);
+    }
+
+    @Override
+    public void startEngine() 
+    {
+        System.out.println("Ford.startEngine() called.");
+    }
+}

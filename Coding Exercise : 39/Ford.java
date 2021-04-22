8ee7537835b020d2d7dc29fc4f5119c32a78de8e
Bhagyesh Hiremath <65004269+bhagyeshiremath@users.noreply.github.com>
2021-04-22 10:56:48 +0530

    Update Ford.java

--- Coding Exercise : 39/Ford.java
+++ Coding Exercise : 39/Ford.java
@@ -10,4 +10,16 @@ public class Ford extends Car
     {
         System.out.println("Ford.startEngine() called.");
     }
+    
+    @Override
+    public void accelerate() 
+    {
+        System.out.println("Ford.accelerate() called.");
+    }
+    
+    @Override
+    public void brake() 
+    {
+        System.out.println("Ford.brake() called.");
+    }
 }
